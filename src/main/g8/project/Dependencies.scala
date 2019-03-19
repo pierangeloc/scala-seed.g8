5263a0dcee9f951d6fa2bf22864dfe0dd5be4ff2
Pierangelo Cecchetto <pierangeloc@gmail.com>
2019-03-19 21:51:22 +0100

    my own settings and typical deps

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -1,5 +1,9 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val scalaTest  = "org.scalatest" %% "scalatest"   % "3.0.5"
+  lazy val cats       = "org.typelevel" %% "cats-core"   % "1.5.0"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "1.1.0"
+  lazy val zio        = "org.scalaz"    %% "scalaz-zio"  % "1.0.0-RC1"
+  lazy val fs2        = "co.fs2"        %% "fs2-io"      % "1.0.0"
 }

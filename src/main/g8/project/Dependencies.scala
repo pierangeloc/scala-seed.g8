971a61fe3c5722dab6f16d6bbe36324afde2b8a8
Pierangelo Cecchetto <pierangeloc@gmail.com>
2019-12-15 14:44:59 +0100

    updated deps and plugins

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -1,9 +1,27 @@
 import sbt._
 
 object Dependencies {
+  val http4sVersion = "0.21.0-M6"
+
+  val zioVersion       = "1.0.0-RC17"
+  val zioCatsVersion   = "2.0.0.0-RC8"
+  val zioNioVersion    = "0.4.0"
+  val zioMacrosVersion = "0.6.0"
+
+  val fs2Version        = "2.0.0"
+  val catsVersion       = "2.0.0"
+  val catsEffectVersion = "2.0.0"
+  val circeVersion      = "0.12.1"
+
   lazy val scalaTest  = "org.scalatest" %% "scalatest"   % "3.0.5"
-  lazy val cats       = "org.typelevel" %% "cats-core"   % "1.5.0"
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "1.1.0"
-  lazy val zio        = "org.scalaz"    %% "scalaz-zio"  % "1.0.0-RC1"
-  lazy val fs2        = "co.fs2"        %% "fs2-io"      % "1.0.0"
+
+  lazy val cats       = "org.typelevel" %% "cats-core"   % catsVersion
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % catsEffectVersion
+  lazy val catsLaws   = "org.typelevel" %% "cats-laws"   % "1.1.0"
+
+  lazy val zio         = "dev.zio"       %% "zio"              % zioVersion
+  lazy val zioStreams  = "dev.zio"       %% "zio-streams"      % zioVersion
+  lazy val zioCats     = "dev.zio"       %% "zio-interop-cats" % zioCatsVersion
+
+  lazy val fs2         = "co.fs2" %% "fs2-io" % fs2Version
 }

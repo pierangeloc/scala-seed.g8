de0158239b82306cc5093467529ed5fc55d61eea
Pierangelo Cecchetto <pierangeloc@gmail.com>
2020-07-11 00:03:42 +0200

    Update Dependencies.scala

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,10 +3,8 @@ import sbt._
 object Dependencies {
   val http4sVersion = "0.21.0-M6"
 
-  val zioVersion       = "1.0.0-RC18"
-  val zioCatsVersion   = "2.0.0.0-RC11"
-  val zioNioVersion    = "0.4.0"
-  val zioMacrosVersion = "0.6.0"
+  val zioVersion       = "1.0.0-RC21-2"
+  val zioCatsVersion   = "2.1.3.0-RC16"
 
   val fs2Version        = "2.1.0"
   val catsVersion       = "2.1.0"
@@ -22,6 +20,8 @@ object Dependencies {
   lazy val zio         = "dev.zio"       %% "zio"              % zioVersion
   lazy val zioStreams  = "dev.zio"       %% "zio-streams"      % zioVersion
   lazy val zioCats     = "dev.zio"       %% "zio-interop-cats" % zioCatsVersion
+  
+  lazy val cask        =  "com.lihaoyi" %% "cask" % "0.6.5"
 
   lazy val fs2         = "co.fs2" %% "fs2-io" % fs2Version
 }

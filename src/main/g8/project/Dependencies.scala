6a778f8bfa741fa4b97c238ceed1af850aeaa09b
Pierangelo Cecchetto <pierangeloc@gmail.com>
2020-10-30 22:02:28 +0100

    Update Dependencies.scala

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,19 +3,18 @@ import sbt._
 object Dependencies {
   val http4sVersion = "0.21.0-M6"
 
-  val zioVersion       = "1.0.0-RC21-2"
-  val zioCatsVersion   = "2.1.3.0-RC16"
+  val zioVersion       = "1.0.3"
+  val zioCatsVersion   = "2.2.0.1"
 
-  val fs2Version        = "2.1.0"
-  val catsVersion       = "2.1.0"
-  val catsEffectVersion = "2.0.0"
+  val fs2Version        = "2.4.4"
+  val catsVersion       = "2.2.0"
+  val catsEffectVersion = "2.2.0"
   val circeVersion      = "0.12.1"
 
   lazy val scalaTest  = "org.scalatest" %% "scalatest"   % "3.0.5"
 
   lazy val cats       = "org.typelevel" %% "cats-core"   % catsVersion
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % catsEffectVersion
-  lazy val catsLaws   = "org.typelevel" %% "cats-laws"   % "1.1.0"
 
   lazy val zio         = "dev.zio"       %% "zio"              % zioVersion
   lazy val zioStreams  = "dev.zio"       %% "zio-streams"      % zioVersion
